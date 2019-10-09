2513b98e84a868cf64a8f64d0fa8849dea865474
Philip Tovstogan <philip.tovstogan@upf.edu>
2019-10-09 17:51:12 +0200

    Make process.py a standalone script

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-AUDIO_DIR = ''
+AUDIO_DIR = '/mnt/data/jamendo-dataset/audio/00/'
 JAMENDO_CLIENT_ID = ''
+EMBEDDINGS_FILE = 'data/embeddings.npy'
+IDS_FILE = 'data/ids.npy'
--- process.py
+++ process.py
@@ -1,13 +1,36 @@
 from pathlib import Path
+import logging
 
-from flask import current_app
 from musicnn.extractor import extractor
+import numpy as np
 
+from config import EMBEDDINGS_FILE, IDS_FILE, AUDIO_DIR
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+ 
 def process():
-    path = Path(current_app.config['AUDIO_DIR'])
+    path = Path(AUDIO_DIR)
     audio_files = sorted(path.glob('**/*.mp3'))[:1]
 
-    for audio_file in audio_files:
+    ids = []
+    embeddings = []
+
+    total = len(audio_files)
+    for i, audio_file in enumerate(audio_files):
         taggram, tags, features = extractor(audio_file, model='MTT_musicnn', extract_features=True)
-        current_app.logger.info(features['penultimate'])
+        features_penultimate = features['penultimate']
+        embeddings.append(features_penultimate[0])
+        ids.append(audio_file.stem)
+        logger.info(f'{i+1}/{total}')
+
+    embeddings = np.array(embeddings, dtype=np.float32)
+    ids = np.array(ids)
+
+    np.save(EMBEDDINGS_FILE, embeddings)
+    np.save(IDS_FILE, ids)
+
+
+if __name__ == '__main__':
+    process()
