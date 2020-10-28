498a0be6cbd414d096f57f075127484450997aaf
Philip Tovstogan <phil.tgun@gmail.com>
2020-10-28 11:47:47 +0100

    Add all config options

--- config-example.py
+++ config-example.py
@@ -1,11 +1,27 @@
-# Flask
-ENV = 'development'
-DEBUG = True
+# Logging
+LOGGING_FORMAT = '%(asctime)s %(levelname)s %(message)s'
+LOGGING_LEVEL = 'DEBUG'
 
-# Data, expects directories in format {dataset}-{model}-{layer}[-pca]
-DATA_DIR = '/path/to/data/jamendo-dataset/'
+# Annoy
+ANNOY_DISTANCE = 'euclidean'
+ANNOY_TREES = 16
 
-# API
-SERVE_AUDIO_LOCALLY = False
+# Models
+MODELS_FILE = 'models.yaml'
+
+# Directories
+ROOT_DIR = '/path/to/root'
+AUDIO_DIR = f'{ROOT_DIR}/audio'  # directory with audio files, can have nested directories
+DATA_DIR = f'{ROOT_DIR}/data'  # directory for extracted embeddings
+INDEX_DIR = f'{ROOT_DIR}/annoy'  # directory for indexed embeddings
+
+# Database
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{ROOT_DIR}/db.sqlite'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Audio
+AUDIO_PROVIDER = 'jamendo'  # can be 'jamendo' for mtg-jamendo-dataset, or 'local' for in-house collection
+
+# Jamendo - ignore if not using Jamendo
 JAMENDO_CLIENT_ID = ''
-
+JAMENDO_METADATA_FILE = ''
