b0eab9bea4d62caa6eae32f0686e1f2c29df9407
Philip Tovstogan <phil.tgun@gmail.com>
2021-05-03 11:19:38 +0200

    Extend hubness experiment to sweep dimensions

--- app/experiments/hubness.py
+++ app/experiments/hubness.py
@@ -1,10 +1,11 @@
-import logging
+import warnings
 
 import click
 import numpy as np
 import pandas as pd
 import plotly.express as px
 from flask.cli import with_appcontext
+from tqdm import tqdm
 
 from app.database.base import Track
 from app.models import get_models
@@ -14,6 +15,8 @@ from .statistics import FLOAT_FORMAT
 RETURN_VALUES = ['k_skewness', 'k_skewness_truncnorm', 'atkinson', 'gini', 'robinhood', 'antihub_occurrence',
                  'hub_occurrence', 'groupie_ratio']
 
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 
 def measure_hubness(n_tracks, output_file, metric, projection, dimensions, n_jobs, random):
     from skhubness import Hubness
@@ -21,17 +24,23 @@ def measure_hubness(n_tracks, output_file, metric, projection, dimensions, n_job
 
     models = get_models()
     models_iter = models.get_combinations() if projection is None else models.get_offline_projections(projection)
-    results = {}
-    for model in models_iter:
-        embeddings = model.get_embeddings_from_file(tracks)
-        embeddings_stacked = np.vstack(embeddings)
+    results = []
+    for model in list(models_iter):
+        for _dimensions in tqdm(range(2, dimensions+1), desc=str(model)):
+            embeddings = model.get_embeddings(tracks, dimensions=slice(_dimensions))
+            embeddings_stacked = np.vstack(embeddings)
 
-        hub = Hubness(k=10, metric=metric, return_value='all', n_jobs=n_jobs)
-        hub.fit(embeddings_stacked[:, :dimensions])
-        results[str(model)] = {key: value for key, value in hub.score().items() if key in RETURN_VALUES}
-        logging.info(f'{model} done')
+            hub = Hubness(k=10, metric=metric, return_value='all', n_jobs=n_jobs)
+            hub.fit(embeddings_stacked[:, :_dimensions])
+            result = {key: value for key, value in hub.score().items() if key in RETURN_VALUES}
+            result.update({
+                'model': str(model),
+                'layer': model.layer,
+                'dimensions': _dimensions
+            })
+            results.append(result)
 
-    results_df = pd.DataFrame.from_dict(results, orient='index')
+    results_df = pd.DataFrame(results)
     results_df.to_csv(output_file, float_format=FLOAT_FORMAT)
 
 
