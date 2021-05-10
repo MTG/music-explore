60efb3678e5dcddea0875d6dded6a67a83196501
Philip Tovstogan <phil.tgun@gmail.com>
2021-05-10 18:42:51 +0200

    Update audio extration script
    - Fix bug where pathlib rglob won't follow symlinks
    - Add ability to specify multiple audio file wildcards

--- app/processing/index_audio.py
+++ app/processing/index_audio.py
@@ -1,4 +1,5 @@
 import logging
+from glob import glob
 from pathlib import Path
 
 import click
@@ -9,17 +10,21 @@ from tqdm import tqdm
 from app.database.base import Track, db, needs_committing
 
 
-def index_audio(input_dir, wildcard='*.mp3'):
+def index_audio(input_dir, wildcards):
     input_dir = Path(input_dir)
-    audio_files = sorted(input_dir.rglob(wildcard))
+    audio_files = []
+    for wildcard in wildcards:
+        audio_files += glob(str(input_dir / '**' / wildcard), recursive=True)
+    audio_files = sorted(audio_files)
     if len(audio_files) == 0:
-        logging.error(f'No {wildcard} files found in {input_dir}')
+        logging.error(f'No {wildcards} files found in {input_dir}')
         exit(1)
+    logging.debug(f'Found {len(audio_files)} audio files')
 
     session_size = 0
     logging.info(f'Indexing audio in {input_dir}')
     for audio_file in tqdm(audio_files):
-        relative_path = str(audio_file.relative_to(input_dir))
+        relative_path = str(Path(audio_file).relative_to(input_dir))
         if Track.get_by_path(relative_path) is None:
             track = Track(path=str(relative_path))
             db.session.add(track)
@@ -33,21 +38,23 @@ def index_audio(input_dir, wildcard='*.mp3'):
     logging.info('Done!')
 
 
-def index_all_audio(wildcard='*.mp3'):
+def index_all_audio(wildcards):
     audio_dir = Path(current_app.config['AUDIO_DIR'])
-    index_audio(audio_dir, wildcard)
+    index_audio(audio_dir, wildcards)
 
 
 @click.command('index-audio')
 @click.argument('input_dir', type=click.Path(exists=True))
-@click.option('-w', '--wildcard', default='*.mp3', help='wildcard that describes the audio files (e.g. *.mp3)')
+@click.option('-w', '--wildcards', default=['*.mp3', '*.flac'], multiple=True,
+              help='wildcards that describes the audio files (e.g. *.mp3)')
 @with_appcontext
-def index_audio_command(input_dir, wildcard):
-    index_audio(input_dir, wildcard)
+def index_audio_command(input_dir, wildcards: tuple):
+    index_audio(input_dir, wildcards)
 
 
 @click.command('index-all-audio')
-@click.option('-w', '--wildcard', default='*.mp3', help='wildcard that describes the audio files (e.g. *.mp3)')
+@click.option('-w', '--wildcards', default=['*.mp3', '*.flac'], multiple=True,
+              help='wildcards that describes the audio files (e.g. *.mp3)')
 @with_appcontext
-def index_all_audio_command(wildcard):
-    index_all_audio(wildcard)
+def index_all_audio_command(wildcards):
+    index_all_audio(wildcards)
